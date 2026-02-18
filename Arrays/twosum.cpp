0e943c25113853940f229d1bbf329f454426ab82
sudhanshhcreates25 <sudhanshs6@gmail.com>
2026-02-18 16:21:01 +0530

    Added Two Sum solution

---
+++ Arrays/twosum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"ENTER THE TWO NUMBERS\n";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"SUM OF GIVEN NUMBERS IS = "<<sum;
+}
